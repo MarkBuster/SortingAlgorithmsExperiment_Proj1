340e03978e0459da92b20cf76fc8132c91161509
Mark Buster <mark.buster@seattlecolleges.edu>
2024-01-27 07:14:15 -0800

    HeapSort implementation

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,2 +1,44 @@
 public class HeapSort {
-}
+    public void heapSort(int array[]) {
+        int length = array.length;
+
+        // Build max heap
+        for (int i = length / 2 - 1; i >= 0; i--) {
+            heapify(array, length, i);
+        }// end for i
+
+        // Extract elements from heap one by one
+        for (int i = length - 1; i > 0; i--) {
+            // Swap root (max element) with the last element
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // Heapify the reduced heap where i is length
+            heapify(array, i, 0);
+        }// end for i
+    }// end heapSort()
+
+    void heapify(int array[], int length, int index) {
+        int largest = index;
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+
+        if (left < length && array[left] > array[largest]) {
+            largest = left;
+        }// end if
+
+        if (right < length && array[right] > array[largest]) {
+            largest = right;
+        }// end if
+
+        if (largest != index) {
+            int swap = array[index];
+            array[index] = array[largest];
+            array[largest] = swap;
+
+            heapify(array, length, largest);
+        }// end if
+    }// end heapify()
+
+}// end class
--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,15 @@ public class Main {
         int[][][] arrayData = dataManager.generateData(5, 2, 14, file);
 
         // You can use this to print the array data.
-        dataManager.printData(arrayData);
+//        dataManager.printData(arrayData);
 
         // Writes the time taken by each algorithm.
-        timeManager.writeAllTimes(arrayData);
+//        timeManager.writeAllTimes(arrayData);
+        int[] testarray = {7,2,89,45,33,72,41,27,45,66,99,180,57,42,23};
+        HeapSort hs = new HeapSort();
+        hs.heapSort(testarray);
+        for (int num : testarray) {
+            System.out.print(num + " ");
+        }
     }
 }
