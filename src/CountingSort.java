f07f6566eff4697638ea273b9ddc7d0ef496860e
KenCage1007 <clenkendrick@aol.com>
2024-01-27 13:06:25 -0800

    Added Counting,Shell,Sorting

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CountingSort {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CountingSort {
+
+    public void countingSort(ArrayList<Integer> arr) {
+        if (arr.size() == 0) {
+            return;
+        }
+        int max = Collections.max(arr);
+        int[] count = new int[max + 1];
+        for (int number : arr) {
+            count[number]++;
+        }
+        int arrIndex = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr.set(arrIndex++, i);
+                count[i]--;
+            }
+        }
+    }
 }
--- src/ShellSort.java
+++ src/ShellSort.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ShellSort {
+
+import java.util.ArrayList;
+
+public class ShellSort {
+
+    public void shellSort(ArrayList<Integer> arr) {
+        int n = arr.size();
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = arr.get(i);
+                int j;
+                for (j = i; j >= gap && arr.get(j - gap) > temp; j -= gap)
+                    arr.set(j, arr.get(j - gap));
+                arr.set(j, temp);
+            }
+        }
+    }
 }
