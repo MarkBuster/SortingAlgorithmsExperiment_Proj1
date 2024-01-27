d206be1946becd650f41977fa11976f7f661d410
Aaron Jeromsky <aaron.k.jeromsky@gmail.com>
2024-01-26 18:02:56 -0800

    Added quick, bubble, and bucket sort to the TimeManager.java, cleaned up bubble and bucket sort.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,18 +1,20 @@
-import java.util.ArrayList;
-
 public class InsertionSort {
-    public void insertionSort(ArrayList<Integer> array) {
-        int size = array.size();
+
+    public void insertionSort(int[] array) {
+
+        int size = array.length;
 
         for (int indexTwo = 1; indexTwo < size; indexTwo++) {
-            int comparedNum = array.get(indexTwo);
+
+            int comparedNum = array[indexTwo];
             int indexOne = indexTwo - 1;
 
-            while (indexOne >= 0 && array.get(indexOne) > comparedNum) {
-                array.set(indexOne + 1, array.get(indexOne));
+            while (indexOne >= 0 && array[indexOne] > comparedNum) {
+
+                array[indexOne + 1] = array[indexOne];
                 indexOne = indexOne - 1;
             }
-            array.set(indexOne + 1, comparedNum);
+            array[indexOne + 1] = comparedNum;
         }
     }
 }
