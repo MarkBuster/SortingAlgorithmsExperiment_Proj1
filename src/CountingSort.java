bf0247a22238691277d212e7362bc2f51a561356
KenCage1007 <clenkendrick@aol.com>
2024-01-31 10:42:05 -0800

    Added Screenshots,and Comments

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -1,18 +1,30 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * The CountingSort class provides a method for performing counting sort on an ArrayList of integers.
+ */
 public class CountingSort {
 
+    /**
+     * Sorts an ArrayList of integers in ascending order using the counting sort algorithm.
+     *
+     * @param arr The ArrayList to be sorted.
+     */
     public void countingSort(ArrayList<Integer> arr) {
         if (arr.size() == 0) {
             return;
         }
+        // Find the maximum value in the input ArrayList
         int max = Collections.max(arr);
+        // Create an array to store counts of each unique element
         int[] count = new int[max + 1];
+        // Count occurrences of each element in the input ArrayList
         for (int number : arr) {
             count[number]++;
         }
         int arrIndex = 0;
+        // Reconstruct the sorted ArrayList based on counts
         for (int i = 0; i < count.length; i++) {
             while (count[i] > 0) {
                 arr.set(arrIndex++, i);
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,19 +1,27 @@
 import java.util.ArrayList;
 
+/**
+ * The SelectionSort class provides a method for performing selection sort on an ArrayList of integers.
+ */
 public class SelectionSort {
 
+    /**
+     * Sorts an ArrayList of integers in ascending order using the selection sort algorithm.
+     *
+     * @param arr The ArrayList to be sorted.
+     */
     public void selectionSort(ArrayList<Integer> arr) {
         int n = arr.size();
 
-
         for (int i = 0; i < n - 1; i++) {
 
+            // Find the index of the minimum element in the unsorted portion of the ArrayList
             int min_idx = i;
             for (int j = i + 1; j < n; j++)
                 if (arr.get(j) < arr.get(min_idx))
                     min_idx = j;
 
-
+            // Swap the minimum element with the current element
             int temp = arr.get(min_idx);
             arr.set(min_idx, arr.get(i));
             arr.set(i, temp);
