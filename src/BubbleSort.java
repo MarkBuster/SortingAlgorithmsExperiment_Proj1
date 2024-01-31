370900bcb688966492d50013014ce289205c89cc
Mark Buster <mark.buster@seattlecolleges.edu>
2024-01-30 22:05:51 -0800

    new output times, Javadoc heap, bubble, bucket

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,5 +1,16 @@
+/**
+ * Bubble sort compares adjacent elements and swaps them if they are in the wrong order. The algorithm continues
+ * to pass through the list until no more swaps are needed, indicating that the list is sorted. During this
+ * process, if two elements have equal keys and are in adjacent positions, they will not be swapped, thereby
+ * remaining a stable algorithm. Bubble sort is not the best algorithm to used with large data sets because of
+ * its time complexity rating of O(n^2). It is an in place sorting algorithm, so it is memory efficient.
+ */
 public class BubbleSort {
-
+    /**
+     * Bubble sort takes in an array and sorts the array in place. The empirical results of this algorithm perfectly
+     * the theoretical results.
+     * @param array array of integers.
+     */
     public void bubbleSort(int[] array) {
 
         int length = array.length;
@@ -18,6 +29,10 @@ public class BubbleSort {
         }
     }
 
+    /**
+     * This method prints an int array that is given to it.
+     * @param array array if integers.
+     */
     public void print(int[] array) {
 
         int length = array.length;
